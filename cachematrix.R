73b322f1db06d7f364eafc751ea30d162ff80774
kcranGitHub <save@mindless.com>
2015-10-17 16:05:03 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        y = solve(x)
 
 }
 
@@ -12,4 +13,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        sol <- x$solve()
+        if(!is.null(sol)) {
+                message("Has already been inverted.  Getting cached data")
+                return(sol)  # Return the previously cached data
+        }
+        
+        # The matrix has not been inverted yet, so we need to invert it
+        data <- x$get()
+        sol <- solve(x, ...)
+        # x$setmean(m)       
+       
+       
+       
+       # y <- solve(x)
+        ##Computing the inverse of a square matrix can be done with the solve function in R.
+        ##For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+
 }
